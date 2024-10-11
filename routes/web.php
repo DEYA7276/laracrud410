963af582136f2d4b3e73c2eb23e92b6d8847db92
DEYA7276 <23610030@utgz.edu.mx>
2024-10-11 13:58:07 -0600

    Project laracrud inicial

--- routes/web.php
+++ routes/web.php
@@ -13,49 +13,9 @@ use App\Http\Controllers\TestController;
 |
 */
 
-Route::get('/start', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/testview', function () {
-    return view('testview');
-});
-
-Route::get('/texto', function () {
-    echo "Hi world";
-    echo "<h1> Hola mundo </h1>";
-});
-
-Route::get('/echo', function () {
-    echo view('test2');
-});
-
-Route::get('/MasRutas', function () {
-    return "Podemos tener las rutas tipo GET, POST, DELETE, PATCH";
-});
-
-
-
-Route::get('/custom', function () {
-    $msj=" Mensaje desde el servidor ";
-    $id=" 500 ";
-    return view('custom',['msj'=>$msj, "identi"=>$id ,"A"=>29]);
-});
-
-Route::get('/contact', function () {
-    return view('contacto');
-}) -> name('contact');
-
-
-
-
-Route::get('/testdb', [TestController::class,'test']);
-
-
-
-
-
-
 
 
 
